b5d99bfebceb1aacf2e40c4398dadd895f77e28b
Mubarak Adogu <Abdulrahmanmubarak98@gmail.com>
2026-05-08 02:42:40 -0700

    Fix render deployment

--- storefront/settings.py
+++ storefront/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-1oxxggut+exlm6isf7zz&#0w^+1g7xb5woi6!!^%82aee0xvt0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
